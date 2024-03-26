ed2fcab4a0f6fbe74e92acd66e8bda7bb097d778
Azuuuna <123644597+Azuuuna@users.noreply.github.com>
2024-03-26 11:57:58 +0100

    Added loading.tpl to optimise the .php pages

--- login.php
+++ login.php
@@ -15,101 +15,17 @@
     <title>Connexion - Internship Etendard</title> 
 </head>
 <body>
-    <div id="loading">
-        <div class="outerspinner">
-            <div class="innerspinner">
-                <?xml version="1.0" encoding="utf-8"?>
-                <svg class="super-etendard" viewBox="35.6009 69.1461 434.5511 294.7583" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 395.498 237.831 C 395.498 237.831 250.137 243.758 203.114 243.412 C 182.083 243.257 146.707 242.754 140.082 240.616 C 137.668 239.837 137.289 232.823 137.553 229.29 C 87.96 229.312 35.555 223.222 35.601 216.4 C 35.525 210.537 88.736 204.186 137.656 203.733 C 137.876 200.541 137.289 193.862 140.517 193.074 C 155.598 189.394 185.257 189.219 202.863 189.59 C 360.341 192.907 361.959 192.408 395.72 194.931"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 203.102 189.477 L 281.634 120.011"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 286.944 119.953 L 276.526 119.779 C 311.4 90.091 322.466 76.973 334.376 71.9 C 346.656 66.669 359.712 69.994 392.495 72.82 L 320.259 182.023 C 318.12 185.679 320.692 192.065 324.728 192.128"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 314.852 216.618 C 314.852 216.618 343.842 211.325 358.396 211.698 C 362.026 211.791 367.544 211.807 376.827 212.219 C 377.914 212.267 374.105 214.203 375.244 214.253 C 406.891 215.66 462.463 216.516 462.463 216.516 C 462.463 216.516 406.095 217.326 374.845 218.923 C 374.39 218.946 377.51 220.764 377.065 220.787 C 368.97 221.202 361.598 221.581 358.222 221.772 C 343.734 222.592 314.852 216.618 314.852 216.618 Z"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <polyline
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        points="387.468 214.677 396.304 210.566 415.081 210.507 415.074 215.316"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <polyline
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        points="387.085 218.432 396.183 222.609 415.09 222.588 415.144 217.892"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 437.395 206.138 C 438.991 208.714 440.921 213.083 440.978 215.466"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 441.051 217.731 C 440.738 219.997 439.108 224.403 437.158 227.379"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 430.565 218.173 L 469.746 273.27 C 423.118 274.615 443.327 279.683 377.166 220.806"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 376.744 212.573 C 441.167 154.232 427.17 159.721 470.152 160.23 L 430.485 214.885"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line
-                        style="stroke: rgb(255, 255, 255); stroke-linejoin: round; stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        x1="203.151" y1="243.822" x2="281.335" y2="312.884"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 286.876 313.109 L 277.228 313.142 C 340.554 369.332 326.308 367.406 392.712 360.342 L 320.641 251.128 C 318.482 247.702 318.182 240.987 324.257 240.508"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 356.913 211.642 L 202.208 210.763 C 176.219 210.621 88.398 200.126 88.538 216.109 C 88.696 234.188 172.069 222.976 202.208 222.751 L 356.777 221.84"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 147.69 207.829 C 153.679 207.863 152.852 225.869 148.002 225.788"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        x1="114.46" y1="214.173" x2="90.759" y2="212.454"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        x1="114.497" y1="218.862" x2="90.542" y2="220.25"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <path
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        d="M 119.755 207.334 C 112.408 208.092 113.642 225.833 119.566 226.269"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line style="stroke: rgb(255, 255, 255); fill-rule: evenodd; fill: none; stroke-width: 7px;" x1="315.486"
-                        y1="216.63" x2="435.787" y2="216.476" transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        x1="137.595" y1="201.121" x2="130.399" y2="201.468"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line
-                        style="stroke: rgb(255, 255, 255); stroke-linecap: round; fill-rule: evenodd; fill: none; stroke-width: 7px;"
-                        x1="137.476" y1="232.051" x2="130.461" y2="231.732"
-                        transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line style="stroke: rgb(255, 255, 255); fill-rule: evenodd; fill: none; stroke-width: 7px;" x1="132.769"
-                        y1="203.503" x2="132.751" y2="202.257" transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                    <line style="stroke: rgb(255, 255, 255); fill-rule: evenodd; fill: none; stroke-width: 7px;" x1="132.926"
-                        y1="231.108" x2="132.902" y2="229.378" transform="matrix(1, 0, 0, 1, 0, -1.4210854715202004e-14)" />
-                </svg>
-            </div>
-        </div>
-    </div>
     
-    <?php
+<?php
         define('SMARTY_DIR', 'C:\Users\Coline\Desktop\myPortfolio\projet\Projet-WEB\libs\\');
         require_once(SMARTY_DIR . 'Smarty.class.php');
+
+        $smarty_loading = new Smarty();
+
+        $smarty_loading->setTemplateDir('tpl/');
+        $smarty_loading->display('loading.tpl');
+
+
 
         $smarty_header = new Smarty();
 
