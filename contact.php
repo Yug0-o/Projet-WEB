f2bb16dbd12e943cc19a9708fadee50cdd91e8f4
Azuuuna <123644597+Azuuuna@users.noreply.github.com>
2024-04-02 10:15:01 +0200

    Corrected a spelling mistake in the file name

--- contact.php
+++ contact.php
@@ -113,7 +113,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="assets/JS/global.js" async></script>
-    <script src="assets/JS/resriction.js" async></script>
+    <script src="assets/JS/restriction.js" async></script>
 </body>
 
 </html>
