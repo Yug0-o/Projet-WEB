12a85076284d1785f71b9d71a5b6dc874a9cb294
Azuuuna <123644597+Azuuuna@users.noreply.github.com>
2024-04-02 11:38:46 +0200

    Changed the vertical-line to fit all pages

--- templates_c/04c7d79b0ff05563a64e28e88b04c10112f0ed0e_0.file.head.tpl.php
+++ templates_c/04c7d79b0ff05563a64e28e88b04c10112f0ed0e_0.file.head.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.48, created on 2024-04-02 10:04:12
+/* Smarty version 3.1.48, created on 2024-04-02 11:28:55
   from 'C:\Users\Coline\Desktop\myPortfolio\projet\Projet-WEB\tpl\head.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.48',
-  'unifunc' => 'content_660bbbfc76f7d2_76657866',
+  'unifunc' => 'content_660bcfd702cfe3_67177191',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '04c7d79b0ff05563a64e28e88b04c10112f0ed0e' => 
     array (
       0 => 'C:\\Users\\Coline\\Desktop\\myPortfolio\\projet\\Projet-WEB\\tpl\\head.tpl',
-      1 => 1712044907,
+      1 => 1712050038,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_660bbbfc76f7d2_76657866 (Smarty_Internal_Template $_smarty_tpl) {
+function content_660bcfd702cfe3_67177191 (Smarty_Internal_Template $_smarty_tpl) {
 ?><head>
         <link rel="preload" href="assets/fonts/GothamSSm-Medium_Web.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="assets/fonts/GothamSSm-Book_Web.woff2" as="font" type="font/woff2" crossorigin>
@@ -28,7 +28,9 @@ function content_660bbbfc76f7d2_76657866 (Smarty_Internal_Template $_smarty_tpl)
         <link rel="preload" href="images/logo-classic.webp" as="image">
         <link rel="icon" href="images/favicon.png">
         <link rel="stylesheet" href="assets/style.css">
+        <link rel="manifest" href="assets/manifest.json">
         <meta charset="utf-8">
+        <meta name="theme-color" content="#F85F6A">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="author" content="Internship Etendard">
         <meta name="description" content="Internship Etendard est une plateforme de recherche de stage pour les étudiants.">
