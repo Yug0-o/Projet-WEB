1e1f3961976c76c69f56ee147b648f15809cd948
Maxandre Coutu <maxandre.coutu@viacesi.fr>
2024-04-03 11:05:24 +0200

    template ?

---
+++ templates_c/662d1ba4c5d44e80796a5c66fa7d5e4068018dbd_0.file.footer.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-04-02 12:04:42
+  from 'C:\www\Projet-WEB\tpl\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_660bd83a3709a9_13394351',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '662d1ba4c5d44e80796a5c66fa7d5e4068018dbd' => 
+    array (
+      0 => 'C:\\www\\Projet-WEB\\tpl\\footer.tpl',
+      1 => 1711464850,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_660bd83a3709a9_13394351 (Smarty_Internal_Template $_smarty_tpl) {
+?><footer>
+        <div class="navbar">
+            <div class="text-center">
+                <a href="homepage.php"> Internship Etendard &copy; <span id="year"></span> </a>
+                <span class="desktop">•</span>
+                <a href="about.php">Tous droits réservés</a>
+                <span class="desktop">•</span>
+                <a href="legalmentions.php">Mentions légales</a>
+                <span class="desktop">•</span>
+                <a href="contact.php">Contact</a>
+            </div>
+        </div>
+    </footer>
+<button class="scroll-top-button hidden" aria-label="Scroll to top"></button><?php }
+}
