7c727074ffb5d460645a99023c3716a33b9d8d5b
the aypisam fpv <the.aypisam.64@gmail.com>
2024-04-03 12:11:15 +0200

    fixed wrong button order in login.php

--- login.php
+++ login.php
@@ -1,19 +1,20 @@
-<!doctype html> 
-<html lang="fr"> 
+<!doctype html>
+<html lang="fr">
 <?php
-    define('SMARTY_DIR', 'libs\\');
-    require_once(SMARTY_DIR . 'Smarty.class.php');
+define('SMARTY_DIR', 'libs\\');
+require_once(SMARTY_DIR . 'Smarty.class.php');
 
-        $smarty_head = new Smarty();
+$smarty_head = new Smarty();
 
-        $smarty_head->assign('dashboard','no');
-        $smarty_head->assign('titre', 'Connexion - Internship Etendard');
-        $smarty_head->setTemplateDir('tpl/');
-        $smarty_head->display('head.tpl');
-    ?>
+$smarty_head->assign('dashboard', 'no');
+$smarty_head->assign('titre', 'Connexion - Internship Etendard');
+$smarty_head->setTemplateDir('tpl/');
+$smarty_head->display('head.tpl');
+?>
+
 <body>
-    
-<?php
+
+    <?php
 
     $smarty_loading = new Smarty();
 
@@ -25,41 +26,41 @@
 
     $smarty_header->setTemplateDir('tpl/');
     $smarty_header->display('header.tpl');
-?>
+    ?>
 
-<main>
-    <section>
-        <div>
-            <h1 class="garde">Connexion</h1>
-        </div>
-        <article> 
-            <form action="check.php" method="post"> <!-- Modification 1 & 2 -->
-                <div class="login-form">
-                    <label for="email">E-mail</label>
-                    <input id="email" name="email" type="email" size="30" value="" required autocomplete="email"> <!-- Modification 3 -->
+    <main>
+        <section>
+            <div>
+                <h1 class="garde">Connexion</h1>
+            </div>
+            <article>
+                <form action="check.php" method="post"> <!-- Modification 1 & 2 -->
+                    <div class="login-form">
+                        <label for="email">E-mail</label>
+                        <input id="email" name="email" type="email" size="30" value="" required autocomplete="email"> <!-- Modification 3 -->
 
-                    <label for="password">Mot de passe</label>
-                    <input id="password" name="password" type="password" size="30" value="" required autocomplete="current_password"> <!-- Modification 3 -->
-                    
-                    <div class="forgot-password">
-                        <a href="password-recovery.php">Mot de passe oublié ?</a>
+                        <label for="password">Mot de passe</label>
+                        <input id="password" name="password" type="password" size="30" value="" required autocomplete="current_password"> <!-- Modification 3 -->
+
+                        <div class="forgot-password">
+                            <a href="password-recovery.php">Mot de passe oublié ?</a>
+                        </div>
+                        <div class="button-container">
+                            <button class="reset" type="reset">Reset</button> <!-- Modification 4 -->
+                            <button class="submit" type="submit">Connexion</button> <!-- Modification 4 -->
+                        </div>
                     </div>
-                    <div class="button-container">
-                        <button class="reset" type="reset">Reset</button> <!-- Modification 4 -->
-                        <button class="submit" type="submit">Connexion</button> <!-- Modification 4 -->
-                    </div>
-                </div>
-            </form>
-        </article>                   
-    </section>
-</main>
+                </form>
+            </article>
+        </section>
+    </main>
 
-<?php
+    <?php
     $smarty_footer = new Smarty();
 
     $smarty_footer->setTemplateDir('tpl/');
     $smarty_footer->display('footer.tpl');
-?>
+    ?>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
--- templates_c/e20bc57ca0cdab18645a7dc65aeddae464eba72d_0.file.head.tpl.php
+++ templates_c/e20bc57ca0cdab18645a7dc65aeddae464eba72d_0.file.head.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.48, created on 2024-04-02 11:00:03
+/* Smarty version 3.1.48, created on 2024-04-03 12:07:56
   from 'D:\Cesi\CPIA2\4 Dev Web\Projet\GitHub\Projet-WEB\tpl\head.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.48',
-  'unifunc' => 'content_660bc91375a307_18111495',
+  'unifunc' => 'content_660d2a7cab8c16_30912552',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e20bc57ca0cdab18645a7dc65aeddae464eba72d' => 
     array (
       0 => 'D:\\Cesi\\CPIA2\\4 Dev Web\\Projet\\GitHub\\Projet-WEB\\tpl\\head.tpl',
-      1 => 1712048215,
+      1 => 1712138871,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,20 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_660bc91375a307_18111495 (Smarty_Internal_Template $_smarty_tpl) {
+function content_660d2a7cab8c16_30912552 (Smarty_Internal_Template $_smarty_tpl) {
 ?><head>
         <link rel="preload" href="assets/fonts/GothamSSm-Medium_Web.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="assets/fonts/GothamSSm-Book_Web.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="assets/fonts/GothamSSm-Light_Web.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="images/logo-classic.webp" as="image">
         <link rel="icon" href="images/favicon.png">
-        <link rel="stylesheet" href="assets/style.css">
+
+        <?php if ($_smarty_tpl->tpl_vars['dashboard']->value == 'yes') {?>
+                <link rel="stylesheet" href="assets/dashboard.css">
+        <?php } else { ?>
+                <link rel="stylesheet" href="assets/style.css">
+        <?php }?>
+
         <link rel="manifest" href="assets/manifest.json">
         <meta charset="utf-8">
         <meta name="theme-color" content="#F85F6A">
